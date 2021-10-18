e0227a49225d4683e14d12ef7f8eae1704832c08
Basic Java Coder <43179251+basicJavaCoder@users.noreply.github.com>
2021-10-18 11:11:18 +0100

    Add files via upload

--- src/MyCalculator/CalcDriver.java
+++ src/MyCalculator/CalcDriver.java
@@ -5,13 +5,22 @@ public class CalcDriver {
  public static void main(String[] args) {
 
   Multiply mult = new Multiply();
-  System.out.println(mult.op(4, 6));
+  System.out.println("Multiplication: " + mult.op(4, 6));
 
   Remainder remain = new Remainder();
-  System.out.println(remain.op(24, 7));
+  System.out.println("Remainder: " + remain.op(24, 7));
 
   Divide newDivide = new Divide();
-  System.out.println(newDivide.op(666, 3));
+  System.out.println("Division: " + newDivide.op(666, 3));
+
+  Subtract newSub = new Subtract();
+  System.out.println("Subtraction: " + newSub.op(333, 100));
+
+  Add newAdd = new Add();
+  System.out.println("Addition: " + newAdd.op(66, 88));
+
+  Percentage newPerc = new Percentage();
+  System.out.println("Percentage: " + newPerc.op(6, 24) + "%");
 
  }
 
